a497719f9aa538a644ea834a39e23820e7c773aa
jesmary885 <jesmary885@gmail.com>
2024-09-10 22:22:17 -0400

    10-9-24

--- app/Http/Livewire/JugarSorteo.php
+++ app/Http/Livewire/JugarSorteo.php
@@ -7,28 +7,40 @@ use App\Models\CartonGanador;
 use App\Models\CartonSorteo;
 use App\Models\Sorteo;
 use App\Models\SorteoFicha;
+use DateTime;
 use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
 
 class JugarSorteo extends Component
 {
-    public $sorteo, $type_1, $type_2, $cont;
-
+    public $carton_ganador, $hoy, $sorteo, $type_1, $type_2, $cont, $sorteo_iniciado = 0, $cartones_sorteo_iniciado;
 
    protected $listeners = ['render' => 'render','echo:sorteo_fichas,NewFichaSorteo' => 'render', 'echo:ganador,NewGanador' => 'render' ];
 
-
-  // protected $listeners = ['render' => 'render','echo:sorteo_fichas,NewFichaSorteo' => 'render'];
-
-    
     public function mount(){
 
         $this->sorteo = Sorteo::where('status','Iniciado')->first();
+    
+        if($this->sorteo){
 
-      /*  $this->type_1 = $this->sorteo->type_1;
+            $this->sorteo_iniciado = 1;
 
-        $this->type_2 = $this->sorteo->type_2;*/
+            $cartones = CartonSorteo::whereHas('sorteo',function(Builder $query){
+                $query->where('id',$this->sorteo->id);
+            })
+            ->where('user_id', auth()->user()->id)
+            ->where('status_pago', 'Pago recibido')
+            ->count(); 
 
+            if($cartones >= 1) $this->cartones_sorteo_iniciado = 1;
+
+            $this->type_1 = $this->sorteo->type_1;
+            $this->type_2 = $this->sorteo->type_2;
+
+        } 
+
+        $fecha_actual = date("Y-m-d H:i:s");
+        $this->hoy= new DateTime($fecha_actual);
     }
 
     public function background($item){
@@ -43,15 +55,6 @@ class JugarSorteo extends Component
             }
 
         }
-
-       /* if($this->cont == 3){
-            CartonGanador::create([
-                'sorteo_id' => $this->sorteo->id,
-                'carton_id' => $carton,
-                'user_id' => auth()->user()->id,
-                'type' => 'Lineal'
-            ]);
-        }*/
     }
 
     public function verifi_linea_horizontal($carton){
@@ -68,27 +71,31 @@ class JugarSorteo extends Component
             foreach(json_decode($fichas_carton->content_1) as $ficha_1){
                 if($ficha_sorteo->numero == $ficha_1) $cont1++;
             }
-
-            foreach(json_decode($fichas_carton->content_2) as $ficha_2){
-                if($ficha_sorteo->numero == $ficha_2) $cont2++;
-            }
-
-            foreach(json_decode($fichas_carton->content_3) as $ficha_3){
-                if($ficha_sorteo->numero == $ficha_3) $cont3++;
-            }
-
-            foreach(json_decode($fichas_carton->content_4) as $ficha_4){
-                if($ficha_sorteo->numero == $ficha_4) $cont4++;
-            }
-
-            foreach(json_decode($fichas_carton->content_5) as $ficha_5){
-                if($ficha_sorteo->numero == $ficha_5) $cont5++;
+            if($cont1 < 5){
+                foreach(json_decode($fichas_carton->content_2) as $ficha_2){
+                    if($ficha_sorteo->numero == $ficha_2) $cont2++;
+                }
+                if($cont2 < 5){
+    
+                    foreach(json_decode($fichas_carton->content_3) as $ficha_3){
+                        if($ficha_sorteo->numero == $ficha_3) $cont3++;
+                    }
+                }
+                if($cont3 < 5){
+                    foreach(json_decode($fichas_carton->content_4) as $ficha_4){
+                        if($ficha_sorteo->numero == $ficha_4) $cont4++;
+                    }
+                    if($cont4 < 5){
+                        foreach(json_decode($fichas_carton->content_5) as $ficha_5){
+                            if($ficha_sorteo->numero == $ficha_5) $cont5++;
+                        }
+                    }
+                }
             }
         }
 
         
         if($cont1 == 5 || $cont2 == 5 || $cont3 == 5 || $cont4 == 5 || $cont5 == 5){
-
             $buscar = CartonGanador::where('sorteo_id',$this->sorteo->id)
                 ->where('carton_id',$carton)
                 ->first();
@@ -98,10 +105,170 @@ class JugarSorteo extends Component
                     'sorteo_id' => $this->sorteo->id,
                     'carton_id' => $carton,
                     'user_id' => auth()->user()->id,
-                    'type' => 'Lineal horizontal'
+                    'type' => 'Lineal'
                 ]);
+
+            $this->carton_ganador = $fichas_carton; 
+
             }
         }
+
+    }
+
+    public function verifi_linea_vertical($carton){
+        $columna1 = 0;
+        $columna2 = 0;
+        $columna3 = 0;
+        $columna4 = 0;
+        $columna5 = 0;
+
+        $fichas_sorteo = SorteoFicha::where('sorteo_id',$this->sorteo->id)->get();
+        $fichas_carton = Carton::where('id',$carton)->first();
+
+        foreach($fichas_sorteo as $ficha_sorteo ){
+
+            if(json_decode($fichas_carton->content_1,true)[0] == $ficha_sorteo->numero )$columna1 ++;
+            if(json_decode($fichas_carton->content_2,true)[0] == $ficha_sorteo->numero )$columna1 ++;
+            if(json_decode($fichas_carton->content_3,true)[0] == $ficha_sorteo->numero )$columna1 ++;
+            if(json_decode($fichas_carton->content_4,true)[0] == $ficha_sorteo->numero )$columna1 ++;
+            if(json_decode($fichas_carton->content_5,true)[0] == $ficha_sorteo->numero )$columna1 ++;
+
+            if(json_decode($fichas_carton->content_1,true)[1] == $ficha_sorteo->numero )$columna2 ++;
+            if(json_decode($fichas_carton->content_2,true)[1] == $ficha_sorteo->numero )$columna2 ++;
+            if(json_decode($fichas_carton->content_3,true)[1] == $ficha_sorteo->numero )$columna2 ++;
+            if(json_decode($fichas_carton->content_4,true)[1] == $ficha_sorteo->numero )$columna2 ++;
+            if(json_decode($fichas_carton->content_5,true)[1] == $ficha_sorteo->numero )$columna2 ++;
+
+            if(json_decode($fichas_carton->content_1,true)[2] == $ficha_sorteo->numero )$columna3 ++;
+            if(json_decode($fichas_carton->content_2,true)[2] == $ficha_sorteo->numero )$columna3 ++;
+            if(json_decode($fichas_carton->content_3,true)[2] == $ficha_sorteo->numero )$columna3 ++;
+            if(json_decode($fichas_carton->content_4,true)[2] == $ficha_sorteo->numero )$columna3 ++;
+            if(json_decode($fichas_carton->content_5,true)[2] == $ficha_sorteo->numero )$columna3 ++;
+
+            if(json_decode($fichas_carton->content_1,true)[3] == $ficha_sorteo->numero )$columna4 ++;
+            if(json_decode($fichas_carton->content_2,true)[3] == $ficha_sorteo->numero )$columna4 ++;
+            if(json_decode($fichas_carton->content_3,true)[3] == $ficha_sorteo->numero )$columna4 ++;
+            if(json_decode($fichas_carton->content_4,true)[3] == $ficha_sorteo->numero )$columna4 ++;
+            if(json_decode($fichas_carton->content_5,true)[3] == $ficha_sorteo->numero )$columna4 ++;
+
+            if(json_decode($fichas_carton->content_1,true)[4] == $ficha_sorteo->numero )$columna5 ++;
+            if(json_decode($fichas_carton->content_2,true)[4] == $ficha_sorteo->numero )$columna5 ++;
+            if(json_decode($fichas_carton->content_3,true)[4] == $ficha_sorteo->numero )$columna5 ++;
+            if(json_decode($fichas_carton->content_4,true)[4] == $ficha_sorteo->numero )$columna5 ++;
+            if(json_decode($fichas_carton->content_5,true)[4] == $ficha_sorteo->numero )$columna5 ++;
+        }
+
+        if($columna1 == 5 || $columna2 == 5 || $columna3 == 5 || $columna4 == 5 || $columna5 == 5){
+            $buscar = CartonGanador::where('sorteo_id',$this->sorteo->id)
+                ->where('carton_id',$carton)
+                ->first();
+
+            if(!$buscar){
+                CartonGanador::create([
+                    'sorteo_id' => $this->sorteo->id,
+                    'carton_id' => $carton,
+                    'user_id' => auth()->user()->id,
+                    'type' => 'Lineal'
+                ]);
+
+                $this->carton_ganador = $fichas_carton; 
+            }
+        }
+
+    
+    
+    }
+
+
+    public function verifi_cuatro_esquinas($carton){
+
+        $fichas_sorteo = SorteoFicha::where('sorteo_id',$this->sorteo->id)->get();
+        $fichas_carton = Carton::where('id',$carton)->first();
+
+        $cont = 0;
+
+        foreach($fichas_sorteo as $ficha_sorteo ){
+
+            if(json_decode($fichas_carton->content_1,true)[0] == $ficha_sorteo->numero )$cont ++;
+            if(json_decode($fichas_carton->content_5,true)[0] == $ficha_sorteo->numero )$cont ++;
+            if(json_decode($fichas_carton->content_1,true)[4] == $ficha_sorteo->numero )$cont ++;
+            if(json_decode($fichas_carton->content_5,true)[4] == $ficha_sorteo->numero )$cont ++;
+
+            if($cont == 4){
+
+                $buscar = CartonGanador::where('sorteo_id',$this->sorteo->id)
+                ->where('carton_id',$carton)
+                ->first();
+
+                if(!$buscar){
+                    CartonGanador::create([
+                        'sorteo_id' => $this->sorteo->id,
+                        'carton_id' => $carton,
+                        'user_id' => auth()->user()->id,
+                        'type' => 'Cuatro esquinas'
+                    ]);
+                }
+
+                $this->carton_ganador = $fichas_carton; 
+            }
+        }
+
+        
+
+    }
+
+    public function verifi_carton_lleno($carton){
+
+        $fichas_sorteo = SorteoFicha::where('sorteo_id',$this->sorteo->id)->get();
+        $fichas_carton = Carton::where('id',$carton)->first();
+
+        $cont = 0;
+
+        foreach($fichas_sorteo as $ficha_sorteo ){
+
+            foreach(json_decode($fichas_carton->content_1) as $ficha_1){
+                if($ficha_sorteo->numero == $ficha_1) $cont++;
+            }
+
+            foreach(json_decode($fichas_carton->content_2) as $ficha_2){
+                if($ficha_sorteo->numero == $ficha_2) $cont++;
+            }
+
+            foreach(json_decode($fichas_carton->content_3) as $ficha_3){
+                if($ficha_sorteo->numero == $ficha_3) $cont++;
+            }
+
+            foreach(json_decode($fichas_carton->content_4) as $ficha_4){
+                if($ficha_sorteo->numero == $ficha_4) $cont++;
+            }
+
+            foreach(json_decode($fichas_carton->content_5) as $ficha_5){
+                if($ficha_sorteo->numero == $ficha_5) $cont++;
+            }
+
+        }
+
+        if($cont == 25){
+
+            $buscar = CartonGanador::where('sorteo_id',$this->sorteo->id)
+            ->where('carton_id',$carton)
+            ->first();
+
+            if(!$buscar){
+
+
+                CartonGanador::create([
+                    'sorteo_id' => $this->sorteo->id,
+                    'carton_id' => $carton,
+                    'user_id' => auth()->user()->id,
+                    'type' => 'Carton lleno'
+                ]);
+            }
+
+            $this->carton_ganador = $fichas_carton; 
+        }
+
+
 
     }
 
@@ -109,31 +276,111 @@ class JugarSorteo extends Component
     
     public function render()
     {
+        if($this->sorteo_iniciado == 1){
+            if($this->cartones_sorteo_iniciado == 1){
 
-        $ganador = 0;
+                $ganador = 0;
 
-        $mis_cartones = CartonSorteo::whereHas('sorteo',function(Builder $query){
-            $query->where('id',$this->sorteo->id);
-        })
-        ->where('user_id', auth()->user()->id)
-        ->where('status_pago', 'Pago recibido')
-        ->get(); 
+                $mis_cartones = CartonSorteo::whereHas('sorteo',function(Builder $query){
+                    $query->where('id',$this->sorteo->id);
+                })
+                ->where('user_id', auth()->user()->id)
+                ->where('status_pago', 'Pago recibido')
+                ->get(); 
 
+                
 
-        $ganadores_sorteo = CartonGanador::where('sorteo_id',$this->sorteo->id)->get();
+                $sorteo_nro = $this->sorteo->id;
 
+                    $ganadores_sorteo = CartonGanador::where('sorteo_id',$this->sorteo->id)->get();
+    
+                    if($ganadores_sorteo->isEmpty() == false){
+                        $ganador = 1;
+                    }
 
+                    $mes_restantes = 0;
+                    $dias_restantes = 0;
+                    $horas_restantes = 0;
+                    $minutos_restantes = 0;
+                    $ano_restantes = 0;
+            }
+            else{
 
-        if($ganadores_sorteo->isEmpty() == false){
+                $cartones_user = CartonSorteo::whereHas('sorteo',function(Builder $query){
+                    $query->where('status','Aperturado');
+                })
+                ->where('user_id', auth()->user()->id)
+                ->where('status_pago', 'Pago recibido')
+                ->first(); 
 
-            $ganador = 1;
+                $sorteo_user_last = Sorteo::where('id',$cartones_user->sorteo_id)->first();
 
+                $proxima_fecha = strtotime($sorteo_user_last->fecha_ejecucion);
+
+                $mes_restantes = date("m",$proxima_fecha);
+                $dias_restantes = date("d",$proxima_fecha);
+                $horas_restantes = date("H",$proxima_fecha);
+                $minutos_restantes = date("I",$proxima_fecha);
+                $ano_restantes = date("Y",$proxima_fecha);
+
+                $mis_cartones = [];
+                $ganador = 0; 
+
+                //$sorteo_user_last = $cartones_user->id;
+
+                $sorteo_nro = $sorteo_user_last->id;
+            }
         }
 
+        else{
 
-        //$fichas_nuevas = SorteoFicha::where('sorteo_id',$this->sorteo->id)->get();
 
-        return view('livewire.jugar-sorteo',compact('mis_cartones','ganador'));
+            $cartones_user = CartonSorteo::whereHas('sorteo',function(Builder $query){
+                $query->where('status','Aperturado');
+            })
+            ->where('user_id', auth()->user()->id)
+            ->where('status_pago', 'Pago recibido')
+            ->first(); 
+
+            if($cartones_user){
+
+                $sorteo_user_last = Sorteo::where('id',$cartones_user->sorteo_id)->first();
+
+                $proxima_fecha = strtotime($sorteo_user_last->fecha_ejecucion);
+
+                $mes_restantes = date("m",$proxima_fecha);
+                $dias_restantes = date("d",$proxima_fecha);
+                $horas_restantes = date("H",$proxima_fecha);
+                $minutos_restantes = date("I",$proxima_fecha);
+                $ano_restantes = date("Y",$proxima_fecha);
+
+                $mis_cartones = [];
+                $ganador = 0;
+
+                //$sorteo_user_last = $cartones_user->id;
+
+                $sorteo_nro = $sorteo_user_last->id;
+            }
+            else{
+
+                $cartones_user = Sorteo::where('status','Aperturado')->first(); 
+
+                $proxima_fecha = strtotime($cartones_user->fecha_ejecucion);
+
+                $mes_restantes = date("m",$proxima_fecha);
+                $dias_restantes = date("d",$proxima_fecha);
+                $horas_restantes = date("H",$proxima_fecha);
+                $minutos_restantes = date("I",$proxima_fecha);
+                $ano_restantes = date("Y",$proxima_fecha);
+
+                $sorteo_nro = $cartones_user->id;
+
+                $mis_cartones = [];
+                $ganador = 0; 
+            }
+        }
+
+        return view('livewire.jugar-sorteo',compact('sorteo_nro','mis_cartones','ganador','ano_restantes','minutos_restantes','mes_restantes','dias_restantes','horas_restantes'));
     }
 
    
--- resources/views/livewire/jugar-sorteo.blade.php
+++ resources/views/livewire/jugar-sorteo.blade.php
@@ -1,70 +1,124 @@
 <div>
 
-    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-
-        @foreach ($mis_cartones as $carton)
-
-            <div class=" bg-white rounded-md shadow-md overflow-hidden">
-                <div class=" bg-blue-500 rounded-t-md shadow-md overflow-hidden md:max-w-xl ">
-                    <div class="grid grid-cols-5 gap-0.5 justify-center mb-1 mt-1">  
-
-                        <div class="bg-blue-600 text-white justify-center text-2xl text-center  py-2  font-bold">B</div>  
-                        <div class="bg-blue-600 text-white justify-center text-2xl text-center  py-2 font-bold">I</div>  
-                        <div class="bg-blue-600 text-white justify-center text-2xl text-center  py-2 font-bold">N</div>  
-                        <div class="bg-blue-600 text-white justify-center text-2xl text-center  py-2 font-bold">G</div>  
-                        <div class="bg-blue-600 text-white justify-center text-2xl text-center py-2 font-bold">O</div>  
-                    </div>  
-
-                    <div class="grid grid-cols-5 gap-0.5 justify-center ml-1 mr-1 mb-1 ">  
-                        @foreach (json_decode($carton->carton->content_1) as $item)
 
 
-                            <div class="{{$this->background($item)}} bg-gray-100 text-lg justify-center text-center py-2 font-bold">  {{$item}}  </div>  
-                        @endforeach
-                    </div>  
+    @if($cartones_sorteo_iniciado == 1)
 
-                    <div class="grid grid-cols-5 gap-0.5 justify-center ml-1 mr-1 mb-1">  
-                        @foreach (json_decode($carton->carton->content_2) as $item)
-                            <div class="{{$this->background($item)}} bg-gray-100  text-lg justify-center text-center py-2 font-bold">{{$item}} </div>  
-                        @endforeach
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+
+            @foreach ($mis_cartones as $carton)
+
+                <div class=" bg-white rounded-md shadow-md overflow-hidden">
+                    <div class=" bg-blue-500 rounded-t-md shadow-md overflow-hidden md:max-w-xl ">
+                        <div class="grid grid-cols-5 gap-0.5 justify-center mb-1 mt-1">  
+
+                            <div class="bg-blue-600 text-white justify-center text-2xl text-center  py-2  font-bold">B</div>  
+                            <div class="bg-blue-600 text-white justify-center text-2xl text-center  py-2 font-bold">I</div>  
+                            <div class="bg-blue-600 text-white justify-center text-2xl text-center  py-2 font-bold">N</div>  
+                            <div class="bg-blue-600 text-white justify-center text-2xl text-center  py-2 font-bold">G</div>  
+                            <div class="bg-blue-600 text-white justify-center text-2xl text-center py-2 font-bold">O</div>  
+                        </div>  
+
+                        <div class="grid grid-cols-5 gap-0.5 justify-center ml-1 mr-1 mb-1 ">  
+                            @foreach (json_decode($carton->carton->content_1) as $item)
+
+
+                                <div class="{{$this->background($item)}} bg-gray-100 text-lg justify-center text-center py-2 font-bold">  {{$item}}  </div>  
+                            @endforeach
+                        </div>  
+
+                        <div class="grid grid-cols-5 gap-0.5 justify-center ml-1 mr-1 mb-1">  
+                            @foreach (json_decode($carton->carton->content_2) as $item)
+                                <div class="{{$this->background($item)}} bg-gray-100  text-lg justify-center text-center py-2 font-bold">{{$item}} </div>  
+                            @endforeach
+                        </div> 
+
+                        <div class="grid grid-cols-5 gap-0.5 justify-center ml-1 mr-1 mb-1">  
+                            @foreach (json_decode($carton->carton->content_3) as $item)
+                                <div class="{{$this->background($item)}} bg-gray-100 text-lg justify-center text-center py-2 font-bold">{{$item}} </div>  
+                            @endforeach
+                        </div> 
+
+                        <div class="grid grid-cols-5 gap-0.5 justify-center ml-1 mr-1 mb-1">  
+                            @foreach (json_decode($carton->carton->content_4) as $item)
+                                <div class="{{$this->background($item)}} bg-gray-100 text-lg justify-center text-center py-2 font-bold">{{$item}} </div>  
+                            @endforeach
+                        </div> 
+
+                        <div class="grid grid-cols-5 gap-0.5 justify-center ml-1 mr-1 mb-1">  
+                            @foreach (json_decode($carton->carton->content_5) as $item)
+                                <div class="{{$this->background($item)}} bg-gray-100 text-lg justify-center text-center py-2 font-bold">{{$item}}</div>  
+                            @endforeach
+                        </div> 
+
+                        <div class="bg-blue-600 m-1 text-center  ">
+                            <p class=" text-white font-semibold text-sm ">SERIAL NRO. {{$carton->carton->serial}}  </p>
+                        </div>
                     </div> 
+                </div>
 
-                    <div class="grid grid-cols-5 gap-0.5 justify-center ml-1 mr-1 mb-1">  
-                        @foreach (json_decode($carton->carton->content_3) as $item)
-                            <div class="{{$this->background($item)}} bg-gray-100 text-lg justify-center text-center py-2 font-bold">{{$item}} </div>  
-                        @endforeach
-                    </div> 
+                @if($type_1 == 'Lineal' || $type_2 == 'Lineal')
+                    {{$this->verifi_linea_horizontal($carton->carton->id)}}
+                    {{$this->verifi_linea_vertical($carton->carton->id)}}
+                @endif
 
-                    <div class="grid grid-cols-5 gap-0.5 justify-center ml-1 mr-1 mb-1">  
-                        @foreach (json_decode($carton->carton->content_4) as $item)
-                            <div class="{{$this->background($item)}} bg-gray-100 text-lg justify-center text-center py-2 font-bold">{{$item}} </div>  
-                        @endforeach
-                    </div> 
+                @if($type_1 == 'Cuatro esquinas' || $type_2 == 'Cuatro esquinas')
+                    {{$this->verifi_cuatro_esquinas($carton->carton->id)}}
+                @endif
 
-                    <div class="grid grid-cols-5 gap-0.5 justify-center ml-1 mr-1 mb-1">  
-                        @foreach (json_decode($carton->carton->content_5) as $item)
-                            <div class="{{$this->background($item)}} bg-gray-100 text-lg justify-center text-center py-2 font-bold">{{$item}}</div>  
-                        @endforeach
-                    </div> 
+                @if($type_1 == 'Carton lleno' || $type_2 == 'Carton lleno')
+                    {{$this->verifi_carton_lleno($carton->carton->id)}}
+                @endif
 
-                    
+            @endforeach
 
-                    <div class="bg-blue-600 m-1 text-center  ">
-                        <p class=" text-white font-semibold text-sm ">SERIAL NRO. {{$carton->carton->serial}}  </p>
-                    </div>
-                </div> 
+        </div>
+
+    @else
+
+        <section class="bg-white dark:bg-gray-900">
+
+            <div class=" flex flex-col space-y-2  justify-center text-center   bg-gray-50 p-10 ">
+                <div class="flex flex-col  justify-center items-center  -translate-x-1/2"> <p class=" text-2xl text-center text-gray-500">En estos momentos no se esta efectuando ningún sorteo en el que te encuestres participando</p>
+                    <h1 class="font-bold  text-2xl text-indigo-500 mt-4 text-center ">
+                        Tiempo restante para iniciar el sorteo Nro. {{$sorteo_nro}}
+                    </h1>
+                    <p class="text-xl text-gray-500 mt-3 "> </p>
+                
+            
+
+                    <p class="text-gray-500 dark:text-gray-300">
+
+                        <div class=" mt-2 " >
+        
+                            <div  id="cuenta">
+            
+            
+                            </div>
+            
+                        </div>
+                        
+                    </p>
+
+                </div>
             </div>
 
-        @endforeach
 
-        {{$this->verifi_linea_horizontal(1)}}
+        </section>
 
-    </div>
+        
 
-    @if($ganador == 1)
-
-        <p>ha ganado el carton </p>
     @endif
+
+        @if($ganador == 1)
+
+            <p>Ha ganado el carton Nro {{$carton_ganador->id}}, con Serial {{$carton_ganador->serial}}</p>
+        @endif
+
+
+            <link rel="stylesheet" href="{{ asset('vendor/css_contador/estilos.css') }}">
+
+ 
 
     <script>
         Echo.channel('ganador')
@@ -72,4 +126,40 @@
             console.log(e);
         });
     </script>
+
+    
+    <script src="{{ asset('vendor/dist/simplyCountdown.min.js') }}"></script>
+
+
+    <script>
+        simplyCountdown('#cuenta', {
+            year: <?php echo $ano_restantes?>, // required
+            month: <?php echo $mes_restantes?>, // required
+            day: <?php echo $dias_restantes?>, // required
+            hours: <?php echo $horas_restantes?>, // Default is 0 [0-23] integer
+            minutes: <?php echo $minutos_restantes?>, // Default is 0 [0-59] integer
+            seconds: 0, // Default is 0 [0-59] integer
+            words: { //words displayed into the countdown
+                days: { singular: 'Día', plural: 'Dias' },
+                hours: { singular: 'Hora', plural: 'Horas' },
+                minutes: { singular: 'Minuto', plural: 'Minutos' },
+                seconds: { singular: 'segundo', plural: 'segundos' }
+            },
+            plural: true, //use plurals
+            inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
+            inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
+            // in case of inline set to false
+            enableUtc: true, //Use UTC as default
+            onEnd: function() {
+                return; 
+            }, //Callback on countdown end, put your own function here
+            refresh: 1000, // default refresh every 1s
+            sectionClass: 'simply-section', //section css class
+            amountClass: 'simply-amount', // amount css class
+            wordClass: 'simply-word', // word css class
+            zeroPad: false,
+            countUp: false
+        });
+    </script>
+
 </div>
