1eac994032ef7d72541647d6a1bebd4904a24a39
jesmary885 <jesmary885@gmail.com>
2024-11-07 19:55:23 -0400

    7-11-24

--- config/adminlte.php
+++ config/adminlte.php
@@ -63,12 +63,12 @@ return [
     |
     */
 
-    'logo' => '<b>Admin</b>LTE',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
-    'logo_img_class' => 'brand-image img-circle elevation-3',
+    'logo' => null,
+    'logo_img' => 'storage/img/logo4.png',
+    'logo_img_class' => 'w-full h-full mx-auto my-0 d-block',
     'logo_img_xl' => null,
-    'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'Admin Logo',
+    'logo_img_xl_class' => null,
+    'logo_img_alt' => null,
 
     /*
     |--------------------------------------------------------------------------
