245263856a49d71e3bee52ce8b498a6790017c49
jesmary885 <jesmary885@gmail.com>
2025-04-08 08:44:27 -0400

    8-4-25

--- resources/views/livewire/jugar-sorteo.blade.php
+++ resources/views/livewire/jugar-sorteo.blade.php
@@ -7035,40 +7035,6 @@
 
        
 
-
-        <script>
-            simplyCountdown('#cuenta', {
-                year: <?php echo $ano_restantes?>, // required
-                month: <?php echo $mes_restantes?>, // required
-                day: <?php echo $dias_restantes?>, // required
-                hours: <?php echo $horas_restantes?>, // Default is 0 [0-23] integer
-                minutes: <?php echo $minutos_restantes?>, // Default is 0 [0-59] integer
-                seconds: 0, // Default is 0 [0-59] integer
-                words: { //words displayed into the countdown
-                    days: { singular: 'Día', plural: 'Dias' },
-                    hours: { singular: 'Hora', plural: 'Horas' },
-                    minutes: { singular: 'Minuto', plural: 'Minutos' },
-                    seconds: { singular: 'segundo', plural: 'segundos' }
-                },
-                plural: true, //use plurals
-                inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
-                inlineClass: 'simply-countdown-inline', //inline css span class in case of inline = true
-                // in case of inline set to false
-                enableUtc: true, //Use UTC as default
-                onEnd: function() {
-                    return;
-                }, //Callback on countdown end, put your own function here
-                refresh: 1000, // default refresh every 1s
-                sectionClass: 'simply-section', //section css class
-                amountClass: 'simply-amount', // amount css class
-                wordClass: 'simply-word', // word css class
-                zeroPad: false,
-                countUp: false
-            });
-        </script>
-
-
-
 <script>
   
         Livewire.on('emitirSonido', function () {
