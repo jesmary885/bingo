bba4fc5fb49e8a0e5a5f1854884f6dc99d78ac0b
jesmary885 <jesmary885@gmail.com>
2025-05-15 10:57:08 -0400

    ss

--- resources/views/livewire/jugar-sorteo.blade.php
+++ resources/views/livewire/jugar-sorteo.blade.php
@@ -5066,7 +5066,7 @@
                             </div> 
 
 
-                             {{-- <div class="w-full bg-white border p-2 border-gray-200 rounded-b-lg shadow ">
+                              <div class="w-full bg-white border p-2 border-gray-200 rounded-b-lg shadow ">
 
                                 <div id="accordion-flush" data-accordion="collapse"
                                     data-active-classes="bg-white dark:bg-gray-900 text-gray-900 dark:text-white"
@@ -5201,7 +5201,7 @@
                                         </div>
                                     </div>
                                 </div>
-                            </div>  --}}
+                            </div> 
 
                         
 
