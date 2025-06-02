a1fa35ec1cfde9327eb808afa37c64c89ce909de
jesmary885 <jesmary885@gmail.com>
2025-06-02 15:29:32 -0400

    s

--- resources/views/livewire/ganancia-sorteo.blade.php
+++ resources/views/livewire/ganancia-sorteo.blade.php
@@ -5,15 +5,15 @@
 
         <div class="flex justify-center ">
 
-            <p class="text-lg text-gray-800 font-bold mr-2" > 
+            <p class="text-sm md:text-lg text-gray-800 font-bold mr-2" > 
                 1er lugar
             </p>
 
-            <p class="text-lg text-gray-800 font-bold" > 
+            <p class="text-sm md:text-lg text-gray-800 font-bold" > 
                 {{$ganancia_dolares}} $
             </p>
         
-            <p class="ml-1 text-lg text-gray-800 font-bold " > 
+            <p class="ml-1 text-sm md:text-lg text-gray-800 font-bold " > 
                 (Bs. {{($ganancia_dolares * $dolar_hoy) }})
             </p>
 
@@ -21,15 +21,15 @@
 
         <div class="flex justify-center ">
 
-            <p class="text-lg text-gray-800 font-bold mr-2" > 
+            <p class="text-sm md:text-lg text-gray-800 font-bold mr-2" > 
                 2do lugar
             </p>
 
-            <p class="text-lg text-gray-800 font-bold" > 
+            <p class="text-sm md:text-lg text-gray-800 font-bold" > 
                 {{$ganancia_dolares_2do}} $
             </p>
         
-            <p class="ml-1 text-lg text-gray-800 font-bold " > 
+            <p class="ml-1 text-sm md:text-lg text-gray-800 font-bold " > 
                 (Bs. {{($ganancia_dolares_2do * $dolar_hoy) }})
             </p>
 
@@ -37,15 +37,15 @@
 
         <div class="flex justify-center ">
 
-            <p class="text-lg text-gray-800 font-bold mr-2" > 
+            <p class="text-sm md:text-lg text-gray-800 font-bold mr-2" > 
                 3er lugar
             </p>
 
-            <p class="text-lg text-gray-800 font-bold" > 
+            <p class="text-sm md:text-lg text-gray-800 font-bold" > 
                 {{$ganancia_dolares_3er}} $
             </p>
         
-            <p class="ml-1 text-lg text-gray-800 font-bold " > 
+            <p class="ml-1 text-sm md:text-lg text-gray-800 font-bold " > 
                 (Bs. {{($ganancia_dolares_3er * $dolar_hoy) }})
             </p>
 
