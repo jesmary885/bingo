1b057ce2f59af9947b8a81c3468f7a9045017be6
jesmary885 <jesmary885@gmail.com>
2025-07-11 21:41:14 -0400

    7-1--25- 5

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5569,7 +5569,7 @@
         
                                     <a class="flex justify-center items-center bg-blue-700 bg-opacity-80 z-10 absolute top-0 left-0 w-full h-full text-white rounded-2xl opacity-0 transition-all duration-300 transform group-hover:scale-105 text-xl group-hover:opacity-100"
                                         href="{{route('cartones.index',['sorteo' => $sorteo->id])}}" target="_self" rel="noopener noreferrer">
-                                        PARTICIPAR
+                                        PARTICIPAR en el 
                                         <svg class="ml-2 w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                                             xmlns="http://www.w3.org/2000/svg">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
